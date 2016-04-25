7f5ab1093e144c435f53a95ab1961700e062c816
Relja Arandjelovic <relja@relja.info>
2016-04-25 12:43:35 +0200

    better setup.m (can run setup.m from another folder)

--- setup.m
+++ setup.m
@@ -5,4 +5,7 @@ run( fullfile(paths.libMatConvNet, 'matlab', 'vl_setupnn.m') );
 addpath( genpath(paths.libReljaMatlab) );
 addpath( genpath(paths.libYaelMatlab) );
 
-addpath('datasets/');
+NetVLADRoot= fileparts(mfilename('fullpath'));
+addpath(NetVLADRoot);
+addpath(fullfile(NetVLADRoot, 'datasets/'));
+clear NetVLADRoot;
