f13271bbd73377de9ee6a665ad3c8b683185625b
Relja Arandjelovic <relja@relja.info>
2017-01-06 19:28:33 +0000

    small fix to Tokyo 24/7 non-max suppression
    The fix improves our results slightly (e.g. 0-0.6%).
    The bug was caused by, for me, surprising behaviour of MATLAB's division operator for int32 types, e.g. int32(9)/10 == 1 instead of expected 0 or 0.9.
    Thanks to WangMao for pointing the bug out.

--- datasets/dbTokyo247.m
+++ datasets/dbTokyo247.m
@@ -16,7 +16,7 @@ classdef dbTokyo247 < dbBase
         function [ids, ds]= nnSearchPostprocess(db, searcher, iQuery, nTop)
             % perform non-max suppression like in Torii et al. CVPR 2015
             [ids, ds]= searcher(iQuery, nTop*12); % 12 cutouts per panorama
-            [~, uniqInd, ~]= unique( floor((ids-1)/12) ,'stable');
+            [~, uniqInd, ~]= unique( idivide(ids-1, 12) ,'stable');
             uniqInd= uniqInd(1:min(end,nTop));
             ids= ids(uniqInd);
             ds= ds(uniqInd);
